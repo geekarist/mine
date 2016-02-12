6721e175f49f5a97e5ff92a34e16b8ed9f4a752e
geekarist <christophe.pele@gmail.com>
2016-02-12 08:24:30 +0100

    Create ListStuffActivity

---
+++ app/src/main/java/com/github/geekarist/mine/list/mvp/ListStuffActivity.java
@@ -0,0 +1,16 @@
+package com.github.geekarist.mine.list.mvp;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+
+import com.github.geekarist.mine.R;
+
+public class ListStuffActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        LayoutInflater.from(this).inflate(R.layout.layout_activity_list_stuff, null);
+    }
+}
