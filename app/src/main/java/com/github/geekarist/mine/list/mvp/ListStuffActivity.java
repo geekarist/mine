3f0d59d30e58eedd4f63d729ee109bfbb559e36d
geekarist <christophe.pele@gmail.com>
2016-02-15 08:18:58 +0100

    liststuff: create recycler view

--- app/src/main/java/com/github/geekarist/mine/list/mvp/ListStuffActivity.java
+++ app/src/main/java/com/github/geekarist/mine/list/mvp/ListStuffActivity.java
@@ -2,7 +2,8 @@ package com.github.geekarist.mine.list.mvp;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.github.geekarist.mine.R;
 
@@ -12,5 +13,7 @@ public class ListStuffActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_activity_list_stuff);
+        RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.list_stuff_recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
