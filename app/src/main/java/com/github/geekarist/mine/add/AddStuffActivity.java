982c1d20911a481e09afcd20d21aed75d7d976b8
geekarist <christophe.pele@gmail.com>
2016-02-19 08:19:35 +0100

    Add description edit field

--- app/src/main/java/com/github/geekarist/mine/add/AddStuffActivity.java
+++ app/src/main/java/com/github/geekarist/mine/add/AddStuffActivity.java
@@ -1,6 +1,15 @@
 package com.github.geekarist.mine.add;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import com.github.geekarist.mine.R;
 
 public class AddStuffActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_activity_add_stuff);
+    }
 }
