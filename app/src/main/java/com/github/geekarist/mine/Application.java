510342a0418d20c24671984faf490a231d5b5bc3
geekarist <christophe.pele@gmail.com>
2016-03-21 08:26:46 +0100

    Fix StrictMode false positive

--- app/src/main/java/com/github/geekarist/mine/Application.java
+++ app/src/main/java/com/github/geekarist/mine/Application.java
@@ -1,12 +1,25 @@
 package com.github.geekarist.mine;
 
 import android.os.StrictMode;
+import android.util.Log;
+
+import com.github.geekarist.mine.edit.EditStuffActivity;
+
+import java.lang.reflect.Method;
 
 public class Application extends android.app.Application {
     @Override
     public void onCreate() {
         if (BuildConfig.DEBUG) {
             StrictMode.enableDefaults();
+            // Fix false positive: see http://stackoverflow.com/a/24617415/1665730
+            Method m;
+            try {
+                m = StrictMode.class.getMethod("incrementExpectedActivityCount", Class.class);
+                m.invoke(null, EditStuffActivity.class);
+            } catch (Exception e) {
+                Log.v(getClass().getSimpleName(), "Exception setting up StrictMode", e);
+            }
         }
         super.onCreate();
     }
